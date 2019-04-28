847b21584a7c70639b390047a3866278755798f3
hang <616431944@qq.com>
2019-04-28 15:54:21 +0800

    sell-project

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <a v-link="{path:'/goods'}">商品</a>
+      </div>
+      <div class="table-item">
+        <a v-link="{path:'/ratings'}">评论</a>
+      </div>
+      <div class="table-item">
+        <a v-link="{path:'/seller'}">商家</a>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from 'components/header/header.vue';
+
+  const ERR_OK = 0;
+
+  export default {
+    data() {
+      return {
+        seller: {}
+      };
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body();
+        if (response.errno === ERR_OR) {
+          this.seller = response.data;
+        }
+      });
+  },
+    components: {
+      'v-header': header
+    }
+  }
+
+</script>
+
+<style lang="stylus" rel="stylesheet">
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7,17,27,0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        text-decortion: none
+        font-size: 14px
+        color: rgb(77, 85, 93)
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ratings"
+    }
+</script>
+
+<style scoped>
+
+</style>

435744b79ee9e60948ea4cafbb04faa8a8492a83
hang <616431944@qq.com>
2019-05-15 22:01:13 +0800

    control

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0"></div>
+    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
-    <div class="cart-add icon-add_circle"></div>
+    <div class="cart-add icon-add_circle" @click="addCart"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Vue from 'vue';
+
   export default {
-  name: "cartcontrol",
-  props: {
-    food: {
-      type: Object
+    name: "cartcontrol",
+    props: {
+      food: {
+        type: Object
+      }
+    },
+      methods: {
+        addCart(event) {
+          if (!event._constructed) {
+            return;
+          }
+          if (!this.food.count) {
+            Vue.set(this.food, 'count', 1);
+          } else {
+            this.food.count++;
+          }
+        },
+        decreaseCart(event) {
+          if (!event._construct) {
+            return;
+          }
+          if (this.food.count) {
+            this.food.count--;
+          }
+      }
     }
-   }
-  }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -27,6 +49,13 @@
         font-size: 24px
       .cart-count
         display: inline-block
+        vertical-align: top
+        width: 12px
+        padding-top: 6px
+        line-height: 24px
+        text-align: center
+        font-size: 10px
+        color: rgb(147, 153, 150)
       .cart-add
         display: inline-block
 </style>
